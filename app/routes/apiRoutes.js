4db70e04d1641451dd4da687ff7d825476f958b3
KKT84 <35786891+kthoma2484@users.noreply.github.com>
2018-08-14 17:50:43 -0700

    -completed compatiblity code

--- app/routes/apiRoutes.js
+++ app/routes/apiRoutes.js
@@ -2,6 +2,7 @@
 // linking routes to a series of "data" sources.
 
 var friendsData = require("../data/friends");
+//let friendNames = ["Jack Napier"];
 
 // ======================== ROUTING==========================
 
@@ -15,9 +16,45 @@ module.exports = function(app) {
 
   // API POST Requests - code handles when a user submits a form subming data to server. 
 
-  app.post("/api/friends", function(req, res) {
+  app.post("/api/friendlist", function(req, res) {
     // Note the code here. Our "server" will respond to requests and 
+
+    let newFriend = req.body.scores
+    let buddyDiff = [];
+
+    for (let i=0; i < friendsData.length; i++) {
+      let friend2 = friendsData[i].scores;
+      let map = newFriend.map(function(x, index) {
+        return Math.abs(x - friend2[index])
+      });
+      console.log(map)
+
+      let reducer = (accumulator, currentValue) => accumulator + currentValue;
+
+      let compatibilityScore = map.reduce(reducer)
+      console.log(compatibilityScore);
+
+      buddyDiff.push(compatibilityScore);
+
+      if (compatibilityScore === Math.min.apply(null,buddyDiff)) {
+        console.log(friendsData[i].name);
+      } else {
+        return null
+      }
+    }
+
     friendsData.push(req.body);
+
+
+    // if (friendNames.indexOf(req.body.name) === 0) {
+    //   //alert("Great job! We're close to finding your perfect friend!")
+    //   console.log("already there");
+
+    // } else {
+    //   friendNames.push(req.body.name);
+    //   console.log("Adding name")
+    //  }
+
   });
 
   app.post("/api/clear", function() {
--- app/routes/htmlRoutes.js
+++ app/routes/htmlRoutes.js
@@ -1,26 +1,37 @@
 // ====================DEPENDENCIES================================
 var path = require("path");
-var express = require("express");
-
-var app = express();
 
 // ======================ROUTING===================================
 
-module.exports = function(app) {
-    // Below code handles when users "visit" a page.
-    app.get("/survey", function(req, res) {
-      res.sendFile(path.join(__dirname, "../public/survey.html"));
+module.exports = function (app) {
+  // Below code handles when users "visit" a page.
+  app.get("/survey", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/survey.html"));
+  });
+
+  // app.post("/survey", function (req, res) {
+  //   res.render("surveyquestions", {
+
+  //   })
+  // });
+
+  app.get("/myfriend", function (req, res) {
+      
+    
+      res.sendFile(path.join(__dirname, "../public/myfriend.html"));
+     
     });
 
-    app.post("/survey", function(req, res) {
+    app.post("/myfriend", function (req, res) {
       res.render("surveyquestions", {
-        
-        })       
-    });
 
-    // If no matching route is found default to home(*)
-    app.get("*", function(req, res) {
-      res.sendFile(path.join(__dirname, "../public/home.html"));
+      })
     });
-  };
+    
+  // If no matching route is found default to home(*)
+  app.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/home.html"));
+  });
 
+  
+};
